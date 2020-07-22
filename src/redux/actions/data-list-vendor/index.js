1662e998f51a4de9d7342141865252baadb34c5c
Maisur <maisur@btoz.co.id>
2020-07-22 09:44:49 +0700

    View List Vendor Item Barang
    #3qv618

---
+++ src/redux/actions/data-list-vendor/index.js
@@ -0,0 +1,89 @@
+import axios from "axios"
+import { api } from "../../config"
+
+export const getData = params => {
+  return async dispatch => {
+    await api.get("/vendor-items", params).then(response => {
+      dispatch({
+        type: "GET_DATA",
+        data: response.data,
+        totalPages: response.data.length,
+        params
+      })
+    })
+  }
+}
+
+export const getInitialData = () => {
+  return async dispatch => {
+    await api.get("/vendor-items").then(response => {
+      dispatch({ type: "GET_ALL_DATA", data: response.data })
+    })
+  }
+}
+
+export const filterData = value => {
+  return dispatch => dispatch({ type: "FILTER_DATA", value })
+}
+
+export const deleteData = obj => {
+  return dispatch => {
+    axios
+      .post("/api/datalist/delete-data", {
+        obj
+      })
+      .then(response => {
+        dispatch({ type: "DELETE_DATA", obj })
+      })
+  }
+}
+
+export const updateData = obj => {
+  return (dispatch, getState) => {
+    axios
+      .post("/api/datalist/update-data", {
+        obj
+      })
+      .then(response => {
+        dispatch({ type: "UPDATE_DATA", obj })
+      })
+  }
+}
+
+export const addData = obj => {
+  return (dispatch, getState) => {
+    let params = getState().dataList.params
+    axios
+      .post("/api/datalist/add-data", {
+        obj
+      })
+      .then(response => {
+        dispatch({ type: "ADD_DATA", obj })
+        dispatch(getData(params))
+      })
+  }
+}
+
+export const addDataItemKategori = obj => {
+  return (dispatch, getState) => {
+    let params = getState().dataList.params
+    if(obj.category === ""){
+      alert("Data tidak boleh kosong")
+    }
+    else{
+      api
+        .post("/item-categories",
+          obj
+        )
+        .then(response => {
+        alert("Kategori berhasil ditambahkan")
+          dispatch({ type: "ADD_DATA", obj })
+          dispatch(getData(params))
+        })
+        .catch(response => {
+          console.log(obj)
+          console.log(response)
+        })
+    }
+  }
+}
